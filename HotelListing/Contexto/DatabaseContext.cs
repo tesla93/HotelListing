db13d4a892ac93576de06c27ca8284ce71c61b67
German Santiesteban <santiestebangerman@gmail.com>
2021-11-24 17:46:31 -0500

    4.1 Implement data Classes and Entity Framework

---
+++ HotelListing/Contexto/DatabaseContext.cs
@@ -0,0 +1,17 @@
+﻿using HotelListing.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace HotelListing.Contexto
+{
+	public class DatabaseContext : DbContext
+	{
+        public DatabaseContext(DbContextOptions options): base(options)
+        {
+        }
+
+        public DbSet<Country> Countries{ get; set; }
+        public DbSet<Hotel> Hotels{ get; set; }
+    }
+}
+
