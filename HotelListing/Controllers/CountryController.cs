083d4b58203c9b18a0af07f45cdb8e3dd5f17153
German Santiesteban <santiestebangerman@gmail.com>
2021-11-25 16:17:33 -0500

    5.3 Construct Get by id endpoint

--- HotelListing/Controllers/CountryController.cs
+++ HotelListing/Controllers/CountryController.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using HotelListing.Data;
 using HotelListing.IRepository;
 using HotelListing.Models;
 using Microsoft.AspNetCore.Mvc;
@@ -17,9 +18,7 @@ namespace HotelListing.Controllers
         private readonly ILogger<CountryController> _logger;
         private readonly IMapper _mapper;
 
-       
-
-        public CountryController(IUnitOfWork unitOfWork, ILogger<CountryController> logger, IMapper mapper)
+       public CountryController(IUnitOfWork unitOfWork, ILogger<CountryController> logger, IMapper mapper)
         {
             _unitOfWork = unitOfWork;
             _logger = logger;
@@ -39,6 +38,21 @@ namespace HotelListing.Controllers
             { 
                 _logger.LogError(ex, $"Something went wrong in the {nameof(GetCountries)}");
                 return StatusCode(500, "Internal server error. Please try again later");                
+            }
+        }
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetCountry(int id)
+        {
+            try
+            {
+                var country = await _unitOfWork.Countries.Get(x=>x.Id==id,new List<string> { nameof(Country.HotelsNavigation)});
+                var result= _mapper.Map<CountryDTO>(country);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Something went wrong in the {nameof(GetCountry)}");
+                return StatusCode(500, "Internal server error. Please try again later");
             }
         }
     }
